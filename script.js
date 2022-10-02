f09b42808bd4b543a5bb3732d1a24b3020d83113
Evgeny <evgeny.lavrov2002@mail.ru>
2022-10-02 22:33:49 +0700

    Add selected days

--- script.js
+++ script.js
@@ -1,3 +1,78 @@
+let selected_days = []
+const SELECTED_CLASS_NAME = "selected"
+
+selectDay = function (event, year, month, day){
+    let date = `${year}-${month}-${day}`
+    let result = event.classList.toggle(SELECTED_CLASS_NAME)
+    if (result) {
+        selected_days.push(date)
+    } else {
+        selected_days.splice(selected_days.indexOf(date), 1)
+    }
+}
+
+selectDaysOfWeek = function (day_number) {
+    let elems = document.querySelectorAll(`tr td.day:nth-child(${day_number})`)
+    let all_deselected = true
+    for (let i = 0; i < elems.length; i++) {
+        if (elems[i].classList.contains(SELECTED_CLASS_NAME)) {
+            all_deselected = false
+            break
+        }
+    }
+    if (all_deselected) {
+        for (let i = 0; i < elems.length; i++) {
+            if (! elems[i].classList.contains(SELECTED_CLASS_NAME)){
+                elems[i].classList.add(SELECTED_CLASS_NAME)
+                let date = elems[i].id.replace("date", "")
+                selected_days.push(date)
+            }
+        }
+    } else {
+        for (let i = 0; i < elems.length; i++) {
+            if (elems[i].classList.contains(SELECTED_CLASS_NAME)){
+                elems[i].classList.remove(SELECTED_CLASS_NAME)
+                let date = elems[i].id.replace("date", "")
+                selected_days.splice(selected_days.indexOf(date), 1)
+            }
+        }
+    }
+}
+
+chooseSelectedDays = function () {
+    for (let i = 0; i < selected_days.length; i++) {
+        let elem = document.querySelector(`#date${selected_days[i]}`)
+        if (elem !== null) {
+            elem.classList.add(SELECTED_CLASS_NAME);
+        }
+    }
+}
+
+let isDrawing = false
+let isDeselecting = false
+startDrawing = function (event) {
+    isDrawing = true
+    isDeselecting = event.classList.contains(SELECTED_CLASS_NAME)
+}
+
+drawing = function (event) {
+    if (isDrawing) {
+        // console.log(event)
+        if (isDeselecting) {
+            let date = event.id.replace("date", "")
+            event.classList.remove(SELECTED_CLASS_NAME)
+            selected_days.splice(selected_days.indexOf(date), 1)
+        } else {
+            let date = event.id.replace("date", "")
+            event.classList.add(SELECTED_CLASS_NAME)
+            selected_days.push(date)
+        }
+    }
+}
+endDrawing = function (event) {
+    isDrawing = false
+}
+
 getWeeksOfMonth = function (year, month) {
     // День недели первого дня месяца
     // [ВС, ПН, ВТ, СР, ЧТ, ПТ, СБ]
@@ -55,9 +130,9 @@ createCalendar = function (year, month){
                 weeks[week][day_of_week] = "<td></td>"
             } else {
                 if (day.getFullYear() === today.getFullYear() && day.getMonth() === today.getMonth() && day.getDate() === today.getDate()){
-                    weeks[week][day_of_week] = `<td id="date${day.getFullYear()}-${day.getMonth() + 1}-${day.getDate()}" class="today day">${day.getDate()}</td>`
+                    weeks[week][day_of_week] = `<td id="date${day.getFullYear()}-${day.getMonth() + 1}-${day.getDate()}" class="today day" onclick="selectDay(this, ${day.getFullYear()}, ${day.getMonth()}, ${day.getDate()})" onmousedown="startDrawing(this)" onmousemove="drawing(this)">${day.getDate()}</td>`
                 } else {
-                    weeks[week][day_of_week] = `<td id="date${day.getFullYear()}-${day.getMonth() + 1}-${day.getDate()}" class="day">${day.getDate()}</td>`
+                    weeks[week][day_of_week] = `<td id="date${day.getFullYear()}-${day.getMonth() + 1}-${day.getDate()}" class="day" onclick="selectDay(this, ${day.getFullYear()}, ${day.getMonth()}, ${day.getDate()})" onmousedown="startDrawing(this)" onmousemove="drawing(this)">${day.getDate()}</td>`
                 }
             }
         }
@@ -71,16 +146,19 @@ createCalendar = function (year, month){
     document.querySelector(".year-month").innerText = `${months[month]} ${year}`
     document.querySelector(".year-month").onclick = function () {
         createCalendar(new Date().getFullYear(), new Date().getMonth())
+        chooseSelectedDays()
     }
 
     let next = new Date(year, month + 1)
     document.querySelector(".next").onclick = function () {
         createCalendar(next.getFullYear(), next.getMonth())
+        chooseSelectedDays()
     }
 
     let prev = new Date(year, month - 1)
     document.querySelector(".prev").onclick = function () {
         createCalendar(prev.getFullYear(), prev.getMonth())
+        chooseSelectedDays()
     }
 }
 
